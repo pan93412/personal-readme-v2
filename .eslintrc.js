88dcf5290e5610510fad7d9e06581d6133461639
pan93412 <pan93412@gmail.com>
2022-10-13 19:20:44 +0800

    Initiate Repository

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ src/hotreload.js
@@ -0,0 +1,9 @@
+/**
+ * Initiate the hot reload function for Parcel.
+ */
+export function initHotReload() {
+  if (module.hot) {
+    module.hot.accept();
+    console.debug("“Hot Reload” functionality has been enabled.");
+  }
+}
