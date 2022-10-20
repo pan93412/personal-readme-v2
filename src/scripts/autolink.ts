54c70da96f6d712226b79c489ee0811c7c6a79bb
pan93412 <pan93412@gmail.com>
2022-10-20 14:49:06 +0800

    refactor!(scripts/autolink): Make this a utility

--- src/scripts/autolink.ts
+++ src/scripts/autolink.ts
@@ -1,5 +1,5 @@
 export function initAutoLink() {
-  document.querySelectorAll("a.h-autolink").forEach((_l) => {
+  document.querySelectorAll("a.u-autolink").forEach((_l) => {
     // The selected `a` elements are all 'HTMLLinkElement'.
     const l = _l as HTMLLinkElement;
     l.href = l.innerText;
