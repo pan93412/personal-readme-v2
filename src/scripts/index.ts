4317eb5b9a8200cf77e2890ccd12bb465964ca6c
pan93412 <pan93412@gmail.com>
2022-10-20 17:49:51 +0800

    refactor: Don't inline the entrance

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,14 @@
-// For tree-shaking.
-import { initHotReload } from "./hotreload";
+export {};
 
-initHotReload();
+declare global {
+  var process: {
+    env: { NODE_ENV: string };
+  };
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  import("./hotreload").then(( { initHotReload }) => initHotReload());
+}
 
 import("./autolink").then((module) => {
   module.initAutoLink();
